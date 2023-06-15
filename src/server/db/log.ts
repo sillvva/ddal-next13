d2874bd65af0282655954e7897222b93bbb2686f
DeKok, Matt G <MGD3@na.valmont.com>
2023-06-15 11:35:50 -0500

    DM Logs Page

--- src/server/db/log.ts
+++ src/server/db/log.ts
@@ -7,3 +7,34 @@ export async function getLog(logId: string) {
 		include: { dm: true, magic_items_gained: true, magic_items_lost: true, story_awards_gained: true, story_awards_lost: true }
 	});
 }
+
+export type DMLogData = Awaited<ReturnType<typeof getDMLogs>>;
+export async function getDMLogs(userId = "", userName = "") {
+	return prisma.log.findMany({
+		where: {
+			is_dm_log: true,
+			dm: {
+				OR: [
+					{
+						uid: userId
+					},
+					{
+						name: userName
+					}
+				]
+			}
+		},
+		include: {
+			dm: true,
+			magic_items_gained: true,
+			magic_items_lost: true,
+			story_awards_gained: true,
+			story_awards_lost: true,
+			character: {
+				include: {
+					user: true
+				}
+			}
+		}
+	});
+}
