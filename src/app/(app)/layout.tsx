efad79a6728b698e9edb0a0252a6c91b8fe5a061
DeKok, Matt G <MGD3@na.valmont.com>
2023-08-02 11:19:52 -0500

    Create ./public alias

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import "$src/app/globals.css";
+import background from "$public/images/barovia-gate.jpg";
 import { HeaderLogin, MenuLogout } from "$src/components/auth";
 import { Drawer } from "$src/components/drawer";
 import { authOptions } from "$src/lib/auth";
@@ -11,7 +12,6 @@ import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 import { mdiGithub } from "@mdi/js";
 import Icon from "@mdi/react";
-import background from "../../../public/images/barovia-gate.jpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
